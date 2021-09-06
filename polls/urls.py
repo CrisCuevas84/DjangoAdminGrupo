806410d2b2cb49b13f92ed2b16500480b8b195d2
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-09-06 19:21:21 -0300

    primer comentario

---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'), # name para acceder desde cualquier enlace
+    path('clientes', views.clientes, name='clientes'),
+    path('sitios', views.sitios, name='sitios'),
+    path('oportunidades', views.oportunidades, name='oportunidades'),
+    path('documentos', views.documentos, name='documentos'),
+    path('libros_publicadores', views.libros_publicadores, name='libros_publicadores'),
+]
---
+++ polls/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import * # Esta importando todos los modelos con el *
+
+
+def index(request):
+    clientes = Client.objects.all()
+    context = {
+        'client_list' : clientes,
+    }
+    return render(request, 'polls/home.html', context)
+
+
+def clientes(request):
+    clientes = Client.objects.all()
+    context = {
+        'client_list' : clientes,
+    }
+    return render(request, 'polls/cliente.html', context)
+
+
+def sitios(request):
+    sitios = Site.objects.all()
+    context = {
+        'sites_list' : sitios,
+    }
+    return render(request, 'polls/sites.html', context)
+
+
+def oportunidades(request):
+    oportunidades = Lead.objects.all()
+    context = {
+        'leads_list' : oportunidades,
+    }
+    return render(request, 'polls/leads.html', context)
+
+
+def documentos(request):
+    documentos = Documento.objects.all()
+    context = {
+        'documents_list' : documentos,
+    }
+    return render(request, 'polls/documents.html', context)
+
+
+def libros_publicadores(request):
+    libros = Libro.objects.all()
+    publicadores = Publicador.objects.all()
+    context = {
+        'lista_libros': libros,
+        'lista_publicadores': publicadores, 
+    }
+    return render(request, 'polls/libros_publicadores.html' ,context)
